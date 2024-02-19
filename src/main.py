2d6dca84500b7bceeb3a3fee361ddb9569d4febb
sarraSlimi <160475297+sarraSlimi@users.noreply.github.com>
2024-02-19 15:30:53 +0100

    Add api and create docker container (#2)
    * calcul des scores de similarités
    
    * Create API
    
    * docker_container_creation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,35 @@
-from similarity.model import Similarity
+from fastapi import FastAPI
+from pydantic import BaseModel
+from .similarity.model import Similarity
+import time
 
+
+app = FastAPI()
 model = Similarity()
-score = model.calculateScore("il aime les chats", "il aime les chiens")
-print(score)
+
+
+class SimilarityCalcul(BaseModel):
+    sentence1: str
+    sentence2: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to sentence Similarity Model"}
+
+
+@app.post("/similarity")
+async def test_similarity(sentences: SimilarityCalcul):
+    start = time.time()
+    sentence1 = sentences.sentence1
+    sentence2 = sentences.sentence2
+    score = model.calculateScore(sentence1, sentence2)
+    compute_time = time.time() - start
+    return {
+        "msg": "we calculated score succesfully",
+        "sentence1": sentence1,
+        "sentence2": sentence2,
+        "score": score,
+        "compute_time": compute_time
+    }
+
